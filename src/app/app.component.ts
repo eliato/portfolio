1aa2444a21f71a816151a13128682cb9bd409f6f
Elias Torres <eliato_@hotmail.com>
2021-08-23 10:49:19 -0600

    update page resume section skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { InfoPaginaService } from './services/info-pagina.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { InfoPaginaService } from 'src/app/services/info-pagina.service';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
